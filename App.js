92a27b4d5a18b72da11d7e9261910364e09b2238
Zilnyk Andrii <azilnyk-c@pivotalcommware.com>
2023-06-13 14:43:59 +0300

    finished with UI

--- App.js
+++ App.js
@@ -9,18 +9,28 @@ import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
 import { GlobalStyles } from './constants/styles';
+import IconButton from './components/UI/IconButton';
+import ExpensesContextProvider from './store/expenses-context';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function ExpensesOverview() {
   return (
-    <Tab.Navigator screenOptions={{
+    <Tab.Navigator screenOptions={({ navigation }) => ({
       headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
       headerTintColor: 'white',
       tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-      tabBarActiveTintColor: GlobalStyles.colors.accent500
-    }}>
+      tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      headerRight: ({ tintColor }) => {
+        return <IconButton
+          icon='add'
+          size={24}
+          color={tintColor}
+          onPress={() => navigation.navigate('ManageExpense')}
+        />
+      }
+    })}>
       <Tab.Screen
         name='RecentExpenses'
         component={RecentExpenses}
@@ -50,19 +60,30 @@ function ExpensesOverview() {
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name='ExpensesOverview'
-            component={ExpensesOverview}
-            options={{
-              headerShown: false
-            }}
-          />
-          <Stack.Screen name='ManageExpense' component={ManageExpense} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <StatusBar style="light" />
+      <ExpensesContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: 'white'
+          }}>
+            <Stack.Screen
+              name='ExpensesOverview'
+              component={ExpensesOverview}
+              options={{
+                headerShown: false
+              }}
+            />
+            <Stack.Screen
+              name='ManageExpense'
+              component={ManageExpense}
+              options={{
+                presentation: 'modal'
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ExpensesContextProvider>
     </>
   );
 }
--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,71 +1,20 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 import { GlobalStyles } from '../../constants/styles';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2023-06-05'),
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2023-06-07'),
-  },
-  {
-    id: 'e3',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2023-06-08'),
-  },
-  {
-    id: 'e4',
-    description: 'A Book',
-    amount: 14.99,
-    date: new Date('2023-06-09'),
-  },
-  {
-    id: 'e5',
-    description: 'Another Book',
-    amount: 18.59,
-    date: new Date('2023-06-10'),
-  },
-  {
-    id: 'e6',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2023-06-07'),
-  },
-  {
-    id: 'e7',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2023-06-08'),
-  },
-  {
-    id: 'e8',
-    description: 'A Book',
-    amount: 14.99,
-    date: new Date('2023-06-09'),
-  },
-  {
-    id: 'e9',
-    description: 'Another Book',
-    amount: 18.59,
-    date: new Date('2023-06-10'),
-  },
-];
+function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>
 
-function ExpensesOutput({ expenses, expensesPeriod }) {
+  if (expenses.length) {
+    content = <ExpensesList expenses={expenses} />;
+  }
+
   return (
     <View style={styles.container}>
-      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      {content}
     </View>
   );
 }
@@ -77,6 +26,12 @@ const styles = StyleSheet.create({
     paddingTop: 24,
     paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+  infoText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 32,
   }
 });
 
